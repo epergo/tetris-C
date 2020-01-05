ed26b1224752230f58cc9efae14d7a3828093200
Eloy <eloy.perez@cabify.com>
2020-01-05 11:52:56 +0100

    Apply CLion autoformat to whole project

--- primlib.h
+++ primlib.h
@@ -3,20 +3,37 @@
 
 #include <SDL.h>
 #include <SDL2_gfxPrimitives.h>
+
 int initGraph();
-enum color {BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX};
+
+enum color {
+    BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX
+};
+
 void pixel(int x, int y, enum color c);
+
 void line(int x1, int y1, int x2, int y2, enum color c);
+
 void circle(int x, int y, int r, enum color c);
+
 void filledRect(int x1, int y1, int x2, int y2, enum color c);
+
 void filledCircle(int x, int y, int r, enum color c);
+
 void rect(int x1, int y1, int x2, int y2, enum color c);
+
 void textout(int x, int y, char *s, enum color c);
+
 int screenWidth();
+
 int screenHeight();
+
 void updateScreen();
+
 int pollkey();
+
 int getkey();
+
 int isKeyDown(int key);
 
 #endif /* __PRIMLIB_H__ */
