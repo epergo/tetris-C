87f7de4e26ebb1548bbb055cb82b954274cc9d9c
Eloy <ej.perezgomez@gmail.com>
2020-01-09 21:58:54 +0100

    Move everything out of main

---
+++ game.h
@@ -0,0 +1,18 @@
+#define DELAY 16
+#define F_COLS 10
+#define F_ROWS 20
+#define VELOCITY 0.5
+
+int play();
+void initField();
+void drawField();
+void drawControlsAndScore();
+void drawNextPiece();
+void movePiece(int);
+void moveLeft();
+void moveRight();
+void rotatePiece();
+void checkIfLine();
+void endGame();
+int setNewPieceInField(int);
+int moveDown();
